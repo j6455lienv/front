56fb7219c192ecc7ef494acd9b633a5242f59c32
Julien Venet <venet.julien@gmail.com>
2018-05-24 11:00:28 +0200

    first 1.5

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
 
   $("#header").load("./components/header/cmp.html");
   $("#page").load("./components/carousel/cmp.html");
-
+  $("#footer").load("./components/footer/cmp.html");
 });
