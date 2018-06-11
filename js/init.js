c1bbe6ceff58daa6c47faa1dc2b96d9229588533
j6455lienv <venet.julien@gmail.com>
2018-06-11 20:43:20 +0200

    dist-linux-enable_apache.conf

--- js/init.js
+++ js/init.js
@@ -10,12 +10,14 @@ function init() {
   //load header and footer components
   $("#header").load("./components/header/cmp.html");
   $("#footer").load("./components/footer/cmp.html");
+  //load the other scripts of the app components scripts and 
+  getScript("components/header/cmp.js");
+  getScript("components/carousel/cmp.js");
+
 
   route('accueil');
 
-  //load the other scripts of the app components scripts and 
-  getScript("components/header/cmp.js");
-  
+ 
   
 }
 
--- js/main.js
+++ js/main.js
@@ -4,32 +4,9 @@
  * 
  */
 
-
-
 $(document).ready(function () {
-
-  /*
-  var hostname = window.location.hostname;
-  var href = window.location.href;
-  
-  console.log(href);
-  
-  var pathname = window.location.pathname;
-  var path = pathname.substring(11, pathname.length);
-  
-  console.log("PathName: " + pathname);
-  console.log("Path: " + path);
-  
-  if(href ==! "http://localhost/nutrimeal/"){
-    console.log("ok path");
-    href === "http://localhost/nutrimeal/";
-  }
-  */
 
   //init first load
   init();
-  
-  
-
-  
+   
 });
