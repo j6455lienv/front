2002881e8cfd02ef7e5c833637eb63fd923f9a6d
j6455lienv <venet.julien@gmail.com>
2018-06-18 11:56:29 +0200

    recette-css ok

--- components/recette-blocb-display/cmp.js
+++ components/recette-blocb-display/cmp.js
@@ -1,10 +1,10 @@
-function getRecette(){
-  hideBlocb("recette-blocb-display",false);
+function getRecette() {
+  hideBlocb("recette-blocb-display", false);
 
   //store values of controll bloc
   var inputRecette = document.getElementById("recette-bloca-input").value;
   var inputNbPersonnes = document.getElementById("recette-bloca-select").value;
-  
+
   /*
   var toDisplay =[
     "id" = null,
@@ -18,17 +18,17 @@ function getRecette(){
   var recetteToDisplay = null;
   var ingredientsToDisplay = null;
   var imgSrcToDisplay = null;
-  
+
   //console.log("recette from display:"+inputRecette+" pour "+inputNbPersonnes+" personnes");
   //console.log(ObjectSize(jsonObject));
   //console.log(jsonObject);
   //console.log(listrecette2);
 
   //use helper ObjectSize to count size of jsonObject
-  for(let i=0; i < ObjectSize(jsonObject);i++){
+  for (let i = 0; i < ObjectSize(jsonObject); i++) {
     //console.log("id => "+jsonObject[i].id + " recette => " + jsonObject[i].recette);
     //compare the input recette with the recettes in jsonObject to find id
-    if(inputRecette === jsonObject[i].recette){
+    if (inputRecette === jsonObject[i].recette) {
       idToDisplay = jsonObject[i].id;
       recetteToDisplay = jsonObject[i].recette;
       ingredientsToDisplay = jsonObject[i].ingredients;
@@ -36,14 +36,14 @@ function getRecette(){
     }
   }
 
-  if(idToDisplay === null){
+  if (idToDisplay === null) {
     errorMessage(inputRecette);
-  }else{
-    displayRecette(idToDisplay,imgSrcToDisplay,recetteToDisplay,ingredientsToDisplay);
+  } else {
+    displayRecette(idToDisplay, imgSrcToDisplay, recetteToDisplay, ingredientsToDisplay);
   }
 }
 
-function displayRecette(idToDisplay,imgSrcToDisplay,recetteToDisplay,ingredientsToDisplay){
+function displayRecette(idToDisplay, imgSrcToDisplay, recetteToDisplay, ingredientsToDisplay) {
 
   //test foreach ok
   //console.log("id find in foreach: " + idToDisplay);
@@ -53,50 +53,51 @@ function displayRecette(idToDisplay,imgSrcToDisplay,recetteToDisplay,ingredients
   //set value in display layout
   var divBlocDetailElt = document.getElementsByClassName("blocTitle")[0];
   var titleElt = document.createElement("h4");
-    titleElt.innerHTML = recetteToDisplay;
+  titleElt.innerHTML = recetteToDisplay;
   divBlocDetailElt.appendChild(titleElt);
 
   //empty div element
   $(".blocImg").html("");
+  //class css no function here
   //set value in display layout
   var divBlocImgElt = document.getElementsByClassName("blocImg")[0];
   var imgElt = document.createElement("img");
-    imgElt.src = imgSrcToDisplay;
-    imgElt.className = "img-responsive img-rounded";
+  imgElt.src = imgSrcToDisplay;
+  imgElt.className = "img-recette";
   divBlocImgElt.appendChild(imgElt);
-  
+
   //empty div element
   $(".blocIngredients").html("");
   //set value in display layout
   var divBlocIngredientsElt = document.getElementsByClassName("blocIngredients")[0];
   var ingredientsElt = document.createElement("p");
-    ingredientsElt.innerHTML = ingredientsToDisplay;
-  divBlocIngredientsElt.appendChild(ingredientsElt); 
+  ingredientsElt.innerHTML = ingredientsToDisplay;
+  divBlocIngredientsElt.appendChild(ingredientsElt);
 }
 
-function errorMessage(inputRecette){
+function errorMessage(inputRecette) {
   //empty bloc divs
   $(".blocImg").html("");
   $(".blocDetail").html("");
   $(".blocIngredients").html("");
-  
+
   //store message
-  var errorMessage = "La recette "+inputRecette+" n'est pas repertoriée dans notre base de données, Veuillez recommencer";
+  var errorMessage = "La recette " + inputRecette + " n'est pas repertoriée dans notre base de données, Veuillez recommencer";
 
   var parentElt = document.getElementsByClassName("blocImg")[0];
   var divElt = document.createElement("div");
-    divElt.className = "alert alert-danger";
-    divElt.innerHTML = errorMessage;
+  divElt.className = "alert alert-danger";
+  divElt.innerHTML = errorMessage;
   var strongElt = document.createElement("strong");
-    strongElt.innerHTML = "Attention ! ";
+  strongElt.innerHTML = "Attention ! ";
 
   //add elements
   divElt.insertBefore(strongElt, divElt.firstChild);
   parentElt.appendChild(divElt);
 
   //disable error message
-  setTimeout(function(){ 
+  setTimeout(function () {
     $(".blocImg").html("");
-    hideBlocb("recette-blocb-display",true);
+    hideBlocb("recette-blocb-display", true);
   }, 3500);
 }
--- js/helper.js
+++ js/helper.js
@@ -1,10 +1,10 @@
 /**
  * filename: /js/helper.js
- * file of helper functions
+ * helper functions
  * 
  */
 
-// 
+// add js scripts in the project
 function getScript(scriptLink) {
   $(document).ready(function () {
     $.getScript('./' + scriptLink, function (data, textStatus, jqxhr) {
@@ -16,17 +16,35 @@ function getScript(scriptLink) {
   });
 }
 
+//return size of an object
+function ObjectSize(obj) {
+  var size = 0,
+    key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++;
+  }
+  return size;
+};
+
+//to hide the bloc who display data informations 
+function hideBlocb(classIdName, boolean) {
+  //disable hidden prop on display div
+  var blocBElt = document.getElementsByClassName(classIdName)[0];
+  blocBElt.hidden = boolean;
+}
+
+
 // AJAX GET
 function ajaxGet(url, callback) {
   var req = new XMLHttpRequest();
   req.open("GET", url);
   req.addEventListener("load", function () {
-      if (req.status >= 200 && req.status < 400) {
-        // Appelle la fonction callback en lui passant la réponse de la requête
-        callback(req.responseText);
-      } else {
-        console.error(req.status + " " + req.statusText + " " + url);
-      }
+    if (req.status >= 200 && req.status < 400) {
+      // Appelle la fonction callback en lui passant la réponse de la requête
+      callback(req.responseText);
+    } else {
+      console.error(req.status + " " + req.statusText + " " + url);
+    }
   });
   req.addEventListener("error", function () {
     console.error("Erreur réseau avec l'URL " + url);
@@ -41,38 +59,21 @@ function ajaxPost(url, data, callback, isJson) {
   var req = new XMLHttpRequest();
   req.open("POST", url);
   req.addEventListener("load", function () {
-      if (req.status >= 200 && req.status < 400) {
-          // Appelle la fonction callback en lui passant la réponse de la requête
-          callback(req.responseText);
-      } else {
-          console.error(req.status + " " + req.statusText + " " + url);
-      }
+    if (req.status >= 200 && req.status < 400) {
+      // Appelle la fonction callback en lui passant la réponse de la requête
+      callback(req.responseText);
+    } else {
+      console.error(req.status + " " + req.statusText + " " + url);
+    }
   });
   req.addEventListener("error", function () {
-      console.error("Erreur réseau avec l'URL " + url);
+    console.error("Erreur réseau avec l'URL " + url);
   });
   if (isJson) {
-      // Définit le contenu de la requête comme étant du JSON
-      req.setRequestHeader("Content-Type", "application/json");
-      // Transforme la donnée du format JSON vers le format texte avant l'envoi
-      data = JSON.stringify(data);
+    // Définit le contenu de la requête comme étant du JSON
+    req.setRequestHeader("Content-Type", "application/json");
+    // Transforme la donnée du format JSON vers le format texte avant l'envoi
+    data = JSON.stringify(data);
   }
   req.send(data);
-}
-
-
-//return size of an object
-function ObjectSize(obj) {
-  var size = 0, key;
-  for (key in obj) {
-      if (obj.hasOwnProperty(key)) size++;
-  }
-  return size;
-};
-
-//to hide the bloc who display data informations 
-function hideBlocb(className ,boolean){
-  //disable hidden prop on display div
-  var blocBElt = document.getElementsByClassName(className)[0];
-  blocBElt.hidden = boolean;
 }
