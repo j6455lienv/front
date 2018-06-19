961f62605d7616fd7dc12da902ec99f80148c667
j6455lienv <venet.julien@gmail.com>
2018-06-19 21:22:52 +0200

    bilan-semaine-delete/edit display and init functions

--- components/bilan_semaine_crud/cmp.js
+++ components/bilan_semaine_crud/cmp.js
@@ -1,5 +1,4 @@
 function createTable(listDateRecette) {
-
   //create Element
   var selectTbodyElt = document.getElementById('tbody-crud');
 
@@ -8,13 +7,30 @@ function createTable(listDateRecette) {
   var newThId = document.createElement('th');
   var newDateTd = document.createElement('td');
   var newRecetteTd = document.createElement('td');
+  var newEditTd = document.createElement('td');
+
+  var editSpan = document.createElement('span');
+  var deleteSpan = document.createElement('span');
 
   for (let i = 0; i < listDateRecette.length; i++) {
+    console.log(listDateRecette[i]);
+
     //param Element
     newThId.scope = "col";
     newThId.innerHTML = i;
     newDateTd.innerHTML = listDateRecette[i].recetteDate;
     newRecetteTd.innerHTML = listDateRecette[i].recetteName;
+
+    // buttons
+    editSpan.innerHTML = '<i class="fas fa-pencil-alt fa-1x"></i>';
+    editSpan.addEventListener("click", function () {
+      editRow(i, listDateRecette[i]);
+    }, false);
+
+    deleteSpan.innerHTML = '<i class="far fa-trash-alt fa-1x"></i>';
+    deleteSpan.addEventListener("click", function () {
+      deleteRow(i, listDateRecette[i]);
+    }, false);
 
     //test value object
     // console.log(listDateRecette[i].recetteDate);
@@ -22,10 +38,26 @@ function createTable(listDateRecette) {
     // console.log(newRecetteTd);
 
     //add child to parent body
+    newEditTd.appendChild(editSpan);
+    newEditTd.appendChild(deleteSpan);
+
     newTr.appendChild(newThId);
     newTr.appendChild(newDateTd);
     newTr.appendChild(newRecetteTd);
+    newTr.appendChild(newEditTd);
     selectTbodyElt.appendChild(newTr);
   }
 
+}
+
+function editRow(indexArray, recetteObj) {
+  alert("edit function ok");
+  console.log("edit function ok");
+  console.log(recetteObj);
+}
+
+function deleteRow(indexArray,recetteObj) {
+  alert("delete function ok");
+  console.log("delete function ok");
+  console.log(recetteObj);
 }
